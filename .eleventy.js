e14332fd510e566017c4c8b0fff24d40ddddd641
IvaTsu <ivan.tsud@gmail.com>
2018-08-23 22:21:37 +0300

    Done base setup. Ver.1.0.0

---
+++ .eleventy.js
@@ -0,0 +1,38 @@
+const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(pluginSyntaxHighlight);
+
+  /* Markdown Plugins */
+  let markdownIt = require("markdown-it");
+  let markdownItAnchor = require("markdown-it-anchor");
+  let options = {
+    html: true,
+    breaks: true,
+    linkify: true
+  };
+  let opts = {
+    permalink: true,
+    permalinkClass: "direct-link",
+    permalinkSymbol: "#"
+  };
+
+  eleventyConfig.setLibrary(
+    "md",
+    markdownIt(options).use(markdownItAnchor, opts)
+  );
+  return {
+    templateFormats: ["md", "html", "pug"],
+
+    // If your site lives in a different subdirectory, change this.
+    // Leading or trailing slashes are all normalized away, so don’t worry about it.
+    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+    // This is only used for URLs (it does not affect your file structure)
+    pathPrefix: "/",
+
+    dir: {
+      input: ".",
+      output: "_site"
+    }
+  };
+};
--- index.md
+++ index.md
@@ -1,1 +1,4 @@
-# header
+---
+layout: base.pug
+title: Home
+---
